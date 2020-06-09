b5c8c57b4248fbe09c9c5e42c03240cbd8976244
lKaza <rodrigoguzman1202@gmail.com>
2020-06-09 16:56:06 -0400

    Clase nueva-frozen al morir Cambiado Player.cs a PlayerController.cs para reflejar mejor que hace

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+        gameObject.SendMessage("Dying");
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,37 +3,44 @@ using System.Collections.Generic;
 using UnityStandardAssets.CrossPlatformInput;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-   
+        [Header("General")]
    [Tooltip("In ms")] [SerializeField] float velocity = 50f;
    [Tooltip("In m")] [SerializeField] float xRange = 13f;
    [Tooltip("In m")] [SerializeField] float yRange = 10f;
-   [Tooltip("")][SerializeField] float pitchFactor = -1f;
-   [SerializeField] float controlPitchFactor = -20f;
+
+        [Header("Position Factors")]
+   [SerializeField] float pitchFactor = -1f;
    [SerializeField] float yawFactor = 1f;
+   
+        [Header("Control Factors")]
+   
    [SerializeField] float controlRollFactor = -50f;
+   [SerializeField] float controlPitchFactor = -20f;
    float horizontalThrow,VerticalThrow;
+   private bool isAlive = true;
 
 
      
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isAlive == true)
+        {
         ShipMovementX();
         ShipMovementY();
         ShipRotation();
+        }
         
     }
 
-    private void OnTriggerEnter(Collider other) {
-        print("dead af by trigger");
-    }
 
     private void ShipRotation(){
         float pitchDueToPosition = transform.localPosition.y * pitchFactor;
@@ -63,5 +70,9 @@ public class Player : MonoBehaviour
         float ClampedPosY = Mathf.Clamp(rawNewYPos, -yRange, yRange);
 
         transform.localPosition = new Vector3(transform.localPosition.x, ClampedPosY, transform.localPosition.z);
+    }
+    public void Dying(){
+        print("shit thats all you had to say negro");
+        isAlive = false;
     }
 }
