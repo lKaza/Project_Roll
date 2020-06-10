02ae43fe6b8e19c8ca9a2df0b6b00255c3444bdc
lKaza <rodrigoguzman1202@gmail.com>
2020-06-09 21:49:58 -0400

    Scoreboard suma con enemigos-TextMeshPro

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerEnemy=20;
+    ScoreBoard scoreBoard;
+    void Start()
+    {
+        AddCollider();  
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddCollider()
+    {
+        Collider myCollider = gameObject.AddComponent<BoxCollider>();
+        myCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other) {
+        GameObject fx = Instantiate(deathFX,transform.position,Quaternion.identity);
+        scoreBoard.ScoreHit(scorePerEnemy);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class ScoreBoard : MonoBehaviour
+{
+    TextMeshProUGUI scoreText;
+    
+    
+    TextContainer m_TextContainer;
+    private void Awake() {
+        scoreText = GetComponent<TextMeshProUGUI>();
+        m_TextContainer = GetComponent<TextContainer>();
+    }
+    int score=0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (scoreText == null)
+            scoreText = gameObject.AddComponent<TextMeshProUGUI>();
+
+        scoreText.text = score.ToString();
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+      
+    }
+    public void ScoreHit(int addedScore){
+        score = score + addedScore;
+        scoreText.text = score.ToString();
+    }
+    public void ScoreBySurviving(){
+        Invoke("ScoreHit",5f);
+    }
+}
